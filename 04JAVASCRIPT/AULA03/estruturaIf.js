e86218fbe32ccbecdf2b4e5a88752148a4e6cc46
Thiago Rocha Lira <rochalirathiago@outlook.com>
2024-05-06 09:37:19 -0300

    Testes com ou/pipe

--- 04JAVASCRIPT/AULA03/estruturaIf.js
+++ 04JAVASCRIPT/AULA03/estruturaIf.js
@@ -1,5 +1,26 @@
-let media =  7;
+// let media = 7;
 
-if (media >= 9) {
-    console.log("Aprovado com Mérito Estudantil.");
+// if (media >= 9) {
+//   console.log("Aprovado com Mérito Estudantil.");
+// } else if (media >= 7) {
+//   console.log("Aluno aprovado.");
+// } else if (media >= 6) {
+//   console.log("Aluno ap. em conselho.");
+// } else {
+//   console.log("Aluno reprovado.");
+// }
+
+// console.log("Fim do programa.");
+
+
+let sol = false;
+let dinheiro = 1412;
+
+
+if (sol || dinheiro >= 1000) {
+  console.log("Vou viajar para a praia.");
+} else if (sol && dinheiro < 1000) {
+  console.log("Vou para o clube.");
+} else {  
+  console.log("Vou ao cinema.");
 }
